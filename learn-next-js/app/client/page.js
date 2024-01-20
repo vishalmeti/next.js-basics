5c435bf1d740393b9de3b1c975a84c16e5a28004
vishalmeti <vishalmeti9@gmail.com>
2024-01-21 02:39:23 +0530

    explaining types of components in comments

--- learn-next-js/app/client/page.js
+++ learn-next-js/app/client/page.js
@@ -1,4 +1,16 @@
 'use client'
+//here we use the above text to tell next.js that let this file be client comppoenet.
+//there are 2 types of components i) server-comps ii) client-comps
+
+//in server comps.. user cant interact with the page thats why the current page cant be server comp as
+//user needs toclick and increment ie user needs to interact with the page hence we making it client comp
+
+// i) server components run in online server and return the result to user.. bcz of that any console.log that are written
+// in server components wont be logged in browser console as the comp will be executed in some 
+// online server, to see such logs we need to check vscode console
+
+//ii) client components run in client device. it works similar to all other basic react framework
+
 import { useState } from "react"
 
 const ClientPage = () => {
