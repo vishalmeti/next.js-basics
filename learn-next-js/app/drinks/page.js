057cff9a3017db0ef71352f148c1231725371a21
vishalmeti <vishalmeti9@gmail.com>
2024-01-21 11:59:51 +0530

    Drinks | Adding error page

--- learn-next-js/app/drinks/page.js
+++ learn-next-js/app/drinks/page.js
@@ -9,6 +9,9 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
 const fetchData = async() =>{
   await new Promise ((resolve)=>setTimeout(resolve,1000))
   const response = await fetch (url)
+if(!response.ok){
+  throw new Error("Failed to fetch drinks")
+}
   const data = await response.json()
   return data;
 }
